df6668ab8405a1a2f9789dfb9e6eefab312b89fb
Naveed <46922100+nedanwr@users.noreply.github.com>
2025-12-15 12:01:15 +0800

    feat(website): update `DocsLayout` to include custom navigation and theme switch options

--- website/src/app/docs/layout.tsx
+++ website/src/app/docs/layout.tsx
@@ -1,10 +1,18 @@
-import { source } from "~/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
-import { baseOptions } from "~/lib/layout.shared";
+
+import { source } from "~/lib/source";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+      nav={{
+        title: "Kronos",
+      }}
+      tree={source.pageTree}
+      themeSwitch={{
+        enabled: false,
+      }}
+    >
       {children}
     </DocsLayout>
   );
